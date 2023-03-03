1cb07160500dd7e6723978a6bd07f038a3d2b031
tuckifyoubuck <aaugtu@gmail.com>
2023-03-03 10:11:49 -0500

    added get_tables_from_dir method which pulls csvs from github directory into pandas dataframes

--- lahman_class.py
+++ lahman_class.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
 
@@ -24,7 +25,7 @@ class Lahman:
     def get_filenames_in_directories(self, dir_list=['core']) -> dict:
         """
         Returns a dictionary containing each directory and filenames in it
-        defaults to returning all directories returned by get_directories method
+        defaults to returning all directories for the 'core' lahman directory
         :param dir_list: directories to pull filenames for
         :return: dict
         """
@@ -39,6 +40,22 @@ class Lahman:
                     dir_files_list.append(i.text)
             dir_files_dict[d] = dir_files_list
         return dir_files_dict
+
+    def get_tables_from_dir(self, tables=None, directory='core'):
+        """
+
+        :param tables: list of tables to return df for
+        :param directory: github directory/folder to pull table/tables from
+        :return: dictionary containing tablename/filename as key and a pandas df with csv data
+        """
+        github_raw_link = 'https://raw.githubusercontent.com/chadwickbureau/baseballdatabank/master/'
+        tables_dict = {}
+        for csv in tables:
+            tbl_name = csv[:-4]
+            tables_dict[tbl_name] = pd.read_csv(github_raw_link+directory+'/'+csv)
+        return tables_dict
+
+
 
 
 
